b7cd837c96882691b800e3485a6f6e123ccbe8c9
wyylovezj <747125491@qq.com>
2025-12-03 10:30:28 +0800

    Merge remote-tracking branch 'origin/master'

--- src/utils/publicData.js
+++ src/utils/publicData.js
@@ -5,10 +5,12 @@ import { ref, computed } from 'vue'
 export const isCollapse = ref(false)
 // 表格数据源
 export const tableData = ref([])
-// 加载状态标志
+// 表格数据查询时加载状态标志
 export const loading = ref(false)
 
+// 登录时加载状态标志
 export const loginLoading = ref(false)
+
 // 同步告警图形动画的标志
 export const blinkTrigger = ref(true)
 
